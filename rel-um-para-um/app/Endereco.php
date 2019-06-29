d638e03c993e9f2e2bb31a07461986f1c3c33029
AndersonPAlmeida <andersonpa10pa@gmail.com>
2019-06-29 01:02:24 -0300

    Aula Sobre Eloquente ORM-Relacionamentos UM PRA UM

---
+++ rel-um-para-um/app/Endereco.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Endereco extends Model
+{
+    function cliente(){
+        return $this->belongsTo('App\Cliente');
+        // return $this->belongsTo('App\Cliente', 'cliente_id', 'id');
+    }
+}
